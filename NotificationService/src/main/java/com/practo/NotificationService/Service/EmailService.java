974dc8fa707801e337d5e5afc8aff8bd7718d24c
Niket-Srivastav <saketabc111@gmail.com>
2025-10-31 16:39:59 +0530

    Create Notification Service

---
+++ NotificationService/src/main/java/com/practo/NotificationService/Service/EmailService.java
@@ -0,0 +1,15 @@
+package com.practo.NotificationService.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.practo.NotificationService.DTO.NotificationEvent;
+
+@Service
+public class EmailService {
+
+    public void SendAppointConfirmationEmail(NotificationEvent notificationEvent) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'SendAppointConfirmationEmail'");
+    }
+    
+}
--- NotificationService/src/main/java/com/practo/NotificationService/Service/NotificationService.java
+++ NotificationService/src/main/java/com/practo/NotificationService/Service/NotificationService.java
@@ -4,12 +4,25 @@ import org.springframework.stereotype.Service;
 
 import com.practo.NotificationService.DTO.NotificationEvent;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class NotificationService {
 
+    private final EmailService emailService;
+
     public void processNotificationEvent(NotificationEvent notificationEvent) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'processNotificationEvent'");
+        try {
+            if(notificationEvent.getRecipientEmail() != null){
+                emailService.SendAppointConfirmationEmail(notificationEvent);
+            }
+        } catch (Exception e) {
+            log.error("Error processing notification event: {}", e.getMessage());
+            throw e;
+        }
     }
     
 }
