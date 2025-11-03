a6b92a2dd83007837fe78368016de06d4ba1323f
Niket-Srivastav <saketabc111@gmail.com>
2025-11-03 16:30:46 +0530

    Configure EmailService

--- NotificationService/src/main/java/com/practo/NotificationService/DTO/NotificationEvent.java
+++ NotificationService/src/main/java/com/practo/NotificationService/DTO/NotificationEvent.java
@@ -1,10 +1,28 @@
 package com.practo.NotificationService.DTO;
 
-public class NotificationEvent {
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data; 
+import lombok.NoArgsConstructor;
 
-    public Object getRecipientEmail() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRecipientEmail'");
-    }
-    
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationEvent {
+    private String eventId;
+    private String recipientEmail;
+    private String doctorName;
+    private String patientName;
+    private LocalTime appointmentTime;
+    private LocalDate appointmentDate;
+    private Double consultationFee;
+    private String appointmentId;
+    private LocalDateTime createdAt;
+    private Map<String, Object> templateData;
 }
+
--- NotificationService/src/main/java/com/practo/NotificationService/Service/EmailService.java
+++ NotificationService/src/main/java/com/practo/NotificationService/Service/EmailService.java
@@ -1,15 +1,55 @@
 package com.practo.NotificationService.Service;
-
+import org.springframework.mail.MailException;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.practo.NotificationService.DTO.NotificationEvent;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+
     public void SendAppointConfirmationEmail(NotificationEvent notificationEvent) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'SendAppointConfirmationEmail'");
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setSubject("New Appointment Confirmed");
+            helper.setTo((String) notificationEvent.getRecipientEmail());
+            helper.setFrom("no-reply@practo.com");
+
+            Context context = new Context();
+            context.setVariable("doctorName", notificationEvent.getDoctorName());
+            context.setVariable("patientName", notificationEvent.getPatientName());
+            context.setVariable("appointmentTime", notificationEvent.getAppointmentTime());
+            context.setVariable("consultationFee", notificationEvent.getConsultationFee());
+            context.setVariable("appointmentId", notificationEvent.getAppointmentId());
+
+            if(notificationEvent.getTemplateData() != null){
+                notificationEvent.getTemplateData().forEach(context::setVariable);
+            }
+
+            String htmlContent = templateEngine.process("appointment-confirmation", context);
+            helper.setText(htmlContent, true);
+
+            javaMailSender.send(message);
+            log.info("Appointment confirmation email sent to {}", notificationEvent.getRecipientEmail());
+
+        } catch (MessagingException | MailException e) {
+            log.error("Error sending appointment confirmation email to {}", notificationEvent.getRecipientEmail(), e);
+        }
     }
     
 }
